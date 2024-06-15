ae25c97256e618786a0e8c9554b9815d761757f6
qutub ahmed <qutubahmed104@gmail.com>
2024-06-15 23:55:45 +0500

    complete

--- app.js
+++ app.js
@@ -3,15 +3,25 @@ const input = document.querySelector("input");
 const ul = document.querySelector("ul");
 submit.addEventListener("click", () => {
   console.log(input.value);
-  const li = document.createElement("li");
-  li.innerHTML = `<input onclick='itemCompleted(this)' type="checkbox" />
-  <p>${input.value}</p>
-  <div>
-    <button>Edit</button>
-    <button>Delete</button>
-  </div>`;
-  ul.appendChild(li);
-  input.value = "";
+  if (input.value == '') {
+    alert('Please insert a value')
+  }
+  else {
+    const li = document.createElement("li");
+    li.innerHTML = `<input onclick='itemCompleted(this)' type="checkbox" />
+    <p class = 'text'>${input.value}</p>
+    <div>
+    <button class = "edit" onclick = "editItem(this)">Edit</button>
+    <button class = "del" onclick="deleteItem(this)">Delete</button>
+    </div>`;
+    ul.appendChild(li);
+    input.value = "";
+    const sound = new Audio();
+    sound.src = 'enter.mp3'
+    sound.play()
+
+
+  }
 });
 
 function itemCompleted(list) {
@@ -20,4 +30,17 @@ function itemCompleted(list) {
   } else {
     list.nextElementSibling.style.textDecoration = "line-through";
   }
+}
+function deleteItem(del) {
+  if (!input.value) {
+    del.parentElement.parentElement.innerHTML = ''
+    li.style.border = 'none';
+    li.style.borderRadius = 'none'
+  }
+}
+
+function editItem(edit){
+  edit.parentElement.parentElement.children[1].innerHTML = '';
+  let editItem = prompt('Enter the value');
+  edit.parentElement.parentElement.children[1].innerHTML = editItem
 }
